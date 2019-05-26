6fae018d07ae8c53586b974e2d0c8b7b5c137053
Laurin Brandner <mail@laurinbrandner.ch>
2019-05-26 17:18:14 +0200

    Carthage compatibility

---
+++ Sourceful.h
@@ -0,0 +1,18 @@
+//
+//  Sourceful.h
+//  Sourceful
+//
+//  Created by Laurin Brandner on 26.05.19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sourceful.
+FOUNDATION_EXPORT double SourcefulVersionNumber;
+
+//! Project version string for Sourceful.
+FOUNDATION_EXPORT const unsigned char SourcefulVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sourceful/PublicHeader.h>
+
+
